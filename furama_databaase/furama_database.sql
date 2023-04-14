7cf75af4550cc0cd4f91c3627c65b485cf95fc91
uthoa1702 <uthoa1702@gmail.com>
2023-04-14 15:31:23 +0700

    Update furama_database.sql

--- furama_databaase/furama_database.sql
+++ furama_databaase/furama_database.sql
@@ -194,7 +194,60 @@ INSERT INTO dich_vu VALUES
 --         OR dia_chi LIKE '%Quảng Trị');
 --  
 --   -- -------------- task 4 -------------
-  
+
+  -- Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần.
+  -- Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng
+  -- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+
+USE furama;
+
+SELECT kh.ho_ten, count(*) AS so_lan_dat
+FROM khach_hang kh
+INNER JOIN  loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
+INNER JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+WHERE lk.ten_loai_khach LIKE 'Diamond'
+GROUP BY kh.ma_khach_hang
+ORDER BY so_lan_dat asc;
+
+
+
+
+
+-- ---------------- task 5 ------------------
+
+--  -Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien
+--  (Với tổng tiền được tính theo công thức như sau: 
+--  Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet)
+--  cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+
+SELECT kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, 
+dv.ten_dich_vu, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc,
+ifnull((dv.chi_phi_thue + ifnull(hdct.so_luong * dvdk.gia, 0) ),0) AS tong_tien
+FROM khach_hang AS kh
+LEFT JOIN hop_dong AS hd ON kh.ma_khach_hang = hd.ma_khach_hang
+LEFT JOIN dich_vu AS dv ON hd.ma_dich_vu = dv.ma_dich_vu
+LEFT JOIN loai_khach AS lk ON lk.ma_loai_khach = kh.ma_loai_khach
+LEFT JOIN hop_dong_chi_tiet AS hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
+LEFT JOIN dich_vu_di_kem AS dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+ORDER BY tong_tien asc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   
   
