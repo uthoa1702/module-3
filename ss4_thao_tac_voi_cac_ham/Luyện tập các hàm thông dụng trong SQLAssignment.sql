a10f374fd00ed5b89c67b33fa3773739fce3919a
uthoa1702 <uthoa1702@gmail.com>
2023-04-17 09:08:20 +0700

    Update Luyện tập các hàm thông dụng trong SQLAssignment.sql

--- ss4_thao_tac_voi_cac_ham/Luyện tập các hàm thông dụng trong SQLAssignment.sql
+++ ss4_thao_tac_voi_cac_ham/Luyện tập các hàm thông dụng trong SQLAssignment.sql
@@ -88,13 +88,14 @@ WHERE m.mark = (SELECT max(m.mark) FROM mark m )
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 
-SELECT studentname, avg(mark)
+SELECT s.studentid, studentname, avg(mark)
 FROM student s
 INNER JOIN mark m 
 ON s.studentid = m.studentid
-GROUP BY markid;
+GROUP BY m.studentid
+ORDER BY avg(mark) DESC, m.studentid;
 
-
+ 
 
 
 
