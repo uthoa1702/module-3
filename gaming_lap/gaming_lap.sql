08bb6c6652147a3951de7ca693274a4c7ffe0256
uthoa1702 <uthoa1702@gmail.com>
2023-05-08 19:29:19 +0700

    ok

---
+++ gaming_lap/gaming_lap.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE gaming_lap;
+USE gaming_lap;
+CREATE TABLE product (
+    id VARCHAR(7),
+    name VARCHAR(250),
+    description VARCHAR(1000),
+    price DOUBLE,
+    brand VARCHAR(250),
+    image VARCHAR(1000)
+)INSERT INTO
