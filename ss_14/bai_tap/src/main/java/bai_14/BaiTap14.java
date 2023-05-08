20c031424bc37c1ab7a37f8e6f1b045d3d0eddef
uthoa1702 <uthoa1702@gmail.com>
2023-05-08 19:26:35 +0700

    ok

---
+++ ss_14/bai_tap/src/main/java/bai_14/BaiTap14.java
@@ -0,0 +1,14 @@
+package bai_14;
+
+public class BaiTap14 {
+    public static void giaiPhuongTrinhBac2(double a, double b, double c) {
+        double delta = b * b - 4 * a * c;
+        if (delta < 0) {
+            System.out.println("Phương trình vô nghiệm!");
+        } else if (delta == 0) {
+            System.out.println("Phương trình có 1 nghiệm kép :" + (-b / 2 * a));
+        } else {
+            System.out.println("Phương trình có 2 nghiệm : " + ((-b + Math.sqrt(delta)) / 2 * a) + " và " + ((-b - Math.sqrt(delta)) / 2 * a));
+        }
+    }
+}
